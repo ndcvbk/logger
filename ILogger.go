75b06368092f75cb92b397cabf3ddb0d30b2a6ff
Robert van der Spek <robert.van.der.spek@ndcmediagroep.nl>
2019-05-10 17:31:21 +0200

    Changes the interface accordingly.

--- ILogger.go
+++ ILogger.go
@@ -3,15 +3,15 @@ package logger
 // ILogger ...
 type ILogger interface {
     // Log with loglevel info
-    Info(interfaceName string, functionName string, message string)
+    Info(message string)
     // Log with loglevel trace
-    Trace(interfaceName string, functionName string, message string)
+    Trace(message string)
     // Log with loglevel debug
-    Debug(interfaceName string, functionName string, message string)
+    Debug(message string)
     // Log with loglevel warn
-    Warn(interfaceName string, functionName string, message string)
+    Warn(message string)
     // Log with loglevel error
-    Error(interfaceName string, functionName string, message string)
+    Error(message string)
     // Log with loglevel fatal
-    Fatal(interfaceName string, functionName string, message string)
+    Fatal(message string)
 }
