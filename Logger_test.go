b02e1153a0b95ac39ca59044334436b3f6dc5d7f
Sander van Rijswijk <sandervanrijswijk@gmail.com>
2020-01-30 15:08:40 +0100

    Add unit tests

--- Logger_test.go
+++ Logger_test.go
@@ -1,55 +1,69 @@
 package logger
 
 import (
-    "github.com/stretchr/testify/suite"
-    "testing"
+	"context"
+	"github.com/stretchr/testify/suite"
+	"testing"
 )
 
 type LoggerTestSuite struct {
-    suite.Suite
+	suite.Suite
 }
 
 func TestSuiteForLogger(t *testing.T) {
-    suite.Run(t, &LoggerTestSuite{})
+	suite.Run(t, &LoggerTestSuite{})
 }
 
 // This test doesn't assert anything at the moment, it's just a quick way to call these functions
 func (suite *LoggerTestSuite) Test() {
-    logger := GetInstance("trace", false)
+	ctx := NewContext(context.Background(), "test")
+	logger := GetInstance("trace", false)
 
-    logger.Trace("testMessage")
-    logger.Trace("test %s", "test")
-    logger.Trace("test %s")
-    logger.Trace("test %d", 1, "extra")
-    logger.Trace("test %d", nil)
+	logger.Trace(nil, "testMessage")
+	logger.Trace(nil, "test %s", "test")
+	logger.Trace(nil, "test %s")
+	logger.Trace(nil, "test %d", 1, "extra")
+	logger.Trace(nil, "test %d", nil)
 
-    var decimal *int
-    decimal = new(int)
-    *decimal = 12
-    var nul *string
-    var boolean *bool
-    boolean = new(bool)
-    *boolean = true
+	var decimal *int
+	decimal = new(int)
+	*decimal = 12
+	var nul *string
+	var boolean *bool
+	boolean = new(bool)
+	*boolean = true
 
-    logger.Trace("test %d %s %s %t", decimal, nul, "testvar", boolean)
+	logger.Trace(nil, "test %d %s %s %t", decimal, nul, "testvar", boolean)
 
-    logger.Debug("testMessage")
-    logger.Debug("test %s", "test")
-    logger.Debug("test %s")
-    logger.Debug("test %d", 1, "extra")
+	logger.Debug(nil, "testMessage")
+	logger.Debug(nil, "test %s", "test")
+	logger.Debug(nil, "test %s")
+	logger.Debug(nil, "test %d", 1, "extra")
 
-    logger.Info("testMessage")
-    logger.Info("test %s", "test")
-    logger.Info("test %s")
-    logger.Info("test %d", 1, "extra")
+	logger.Info(nil, "testMessage")
+	logger.Info(nil, "test %s", "test")
+	logger.Info(nil, "test %s")
+	logger.Info(nil, "test %d", 1, "extra")
 
-    logger.Warn("testMessage")
-    logger.Warn("test %s", "test")
-    logger.Warn("test %s")
-    logger.Warn("test %d", 1, "extra")
+	logger.Warn(nil, "testMessage")
+	logger.Warn(nil, "test %s", "test")
+	logger.Warn(nil, "test %s")
+	logger.Warn(nil, "test %d", 1, "extra")
 
-    logger.Error("testMessage")
-    logger.Error("test %s", "test")
-    logger.Error("test %s")
-    logger.Error("test %d", 1, "extra")
+	logger.Error(nil, "testMessage")
+	logger.Error(nil, "test %s", "test")
+	logger.Error(nil, "test %s")
+	logger.Error(nil, "test %d", 1, "extra")
+
+	logger.Trace(ctx, "testMessage")
+	logger.Debug(ctx, "testMessage")
+	logger.Info(ctx, "testMessage")
+	logger.Warn(ctx, "testMessage")
+	logger.Error(ctx, "testMessage")
+
+	logger.Trace(context.Background(), "testMessage")
+	logger.Debug(context.Background(), "testMessage")
+	logger.Info(context.Background(), "testMessage")
+	logger.Warn(context.Background(), "testMessage")
+	logger.Error(context.Background(), "testMessage")
 }
